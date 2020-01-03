3120c6253cf2e00303c2cde205dc7acd60de3f12
MrJanD <40394719+MrJanD@users.noreply.github.com>
2020-01-03 21:55:40 +0100

    Tested and working as expected...

--- mqtt_radiator.py
+++ mqtt_radiator.py
@@ -74,10 +74,12 @@ class MQTT_Handler(object):
         self.mqtt_sub_kitchen = "Kitchen/Radiator/set"
         self.mqtt_sub_diningroom = "Diningroom/Radiator/set"
         self.mqtt_sub_livingroom = "Livingroom/Radiator/set"
+        self.mqtt_sub_pump = "Server/Radiator/Pump/set"
+        self.mqtt_sub_request = "Server/Radiator/get"
         self.mqtt_pub_kitchen = "Kitchen/Radiator/state"
         self.mqtt_pub_diningroom = "Diningroom/Radiator/state"
         self.mqtt_pub_livingroom = "Livingroom/Radiator/state"
-        self.mqtt_sub_request = "Server/Radiator/get"
+        self.mqtt_pub_pump = "Server/Radiator/Pump/state"
         self.mqtt_pub_flow = "Server/Radiator/flow/temperature"
         self.mqtt_pub_return = "Server/Radiator/return/temperature"
         self.mqtt_pub_state = "Server/Radiator/state"
@@ -86,6 +88,7 @@ class MQTT_Handler(object):
         self.kitchen_tuple = GPIO_Tuple(24, 12, inverse = True)
         self.diningroom_tuple = GPIO_Tuple(25, 16, inverse = True)
         self.livingroom_tuple = GPIO_Tuple(8, 20, inverse = True)
+        self.pump_tuple = GPIO_Tuple(15, 18, inverse = True)
         self.power_led = Power_LED(7)
         
         self.temperature = Temperature_Handler()
@@ -96,9 +99,11 @@ class MQTT_Handler(object):
         client.subscribe(self.mqtt_sub_diningroom)
         client.subscribe(self.mqtt_sub_livingroom)
         client.subscribe(self.mqtt_sub_request)
+        client.subscribe(self.mqtt_sub_pump)
         client.message_callback_add(self.mqtt_sub_kitchen, self.kitchen_radiator)
         client.message_callback_add(self.mqtt_sub_diningroom, self.diningroom_radiator)
         client.message_callback_add(self.mqtt_sub_livingroom, self.livingroom_radiator)
+        client.message_callback_add(self.mqtt_sub_pump, self.pump_radiator)
         client.message_callback_add(self.mqtt_sub_request, self.radiator_state)
         client.publish(self.mqtt_pub_state, "Online", retain = True)
         self.power_led.on()
@@ -122,18 +127,23 @@ class MQTT_Handler(object):
 
     def kitchen_radiator(self, client, userdata, message):
         print("kitchen_radiator: " + message.topic + " " + str(message.payload))
-        self.kitchen_tuple.set_gpio("ON" in str(message.payload))
-        client.publish(self.mqtt_pub_kitchen, self.get_tuple_mqtt_state(self.kitchen_tuple), retain = True)
+        self.kitchen_tuple.set_gpio("on" in str(message.payload))
+        client.publish(self.mqtt_pub_kitchen, self.get_tuple_mqtt_state(self.kitchen_tuple))
 
     def diningroom_radiator(self, client, userdata, message):
         print("diningroom_radiator: " + message.topic + " " + str(message.payload))
-        self.diningroom_tuple.set_gpio("ON" in str(message.payload))
-        client.publish(self.mqtt_pub_diningroom, self.get_tuple_mqtt_state(self.diningroom_tuple), retain = True)
+        self.diningroom_tuple.set_gpio("on" in str(message.payload))
+        client.publish(self.mqtt_pub_diningroom, self.get_tuple_mqtt_state(self.diningroom_tuple))
 
     def livingroom_radiator(self, client, userdata, message):
         print("livingroom_radiator: " + message.topic + " " + str(message.payload))
-        self.livingroom_tuple.set_gpio("ON" in str(message.payload))
-        client.publish(self.mqtt_pub_livingroom, self.get_tuple_mqtt_state(self.livingroom_tuple), retain = True)
+        self.livingroom_tuple.set_gpio("on" in str(message.payload))
+        client.publish(self.mqtt_pub_livingroom, self.get_tuple_mqtt_state(self.livingroom_tuple))
+        
+    def pump_radiator(self, client, userdata, message):
+        print("pump_radiator: " + message.topic + " " + str(message.payload))
+        self.pump_tuple.set_gpio("on" in str(message.payload))
+        client.publish(self.mqtt_pub_pump, self.get_tuple_mqtt_state(self.pump_tuple))
 
     def radiator_state(self, client, userdata, message):
         flow_temp, return_temp = self.temperature.get_temps()
@@ -144,9 +154,9 @@ class MQTT_Handler(object):
 
     def get_tuple_mqtt_state(self, tuple):
         if tuple.get_gpio():
-            return "ON"
+            return "on" if not tuple.inverse else "off"
         else:
-            return "OFF"
+            return "off" if not tuple.inverse else "on"
 
     def run_temperature_emitter(self):
         while True:
